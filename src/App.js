cf864f8c78a4c77796f789edc2de8cecbbd86324
Lance Larsen <lancelarsen2001@gmail.com>
2022-05-24 15:17:16 -0600

    lances changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Row from './Row'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h3 className='title'>Tic, Tac, Toe.</h3>
+      <div className='game'>
+        <Row></Row>
+        <Row></Row>
+        <Row></Row>
+      </div>
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+const Row = ()=>{
+function colm1() {
+
+}
+    function colm2() {
+    }
+
+function colm3(){
+    
+}
+    return (
+        <div className='Colms'>
+    <button onClick={colm1}> </button>
+
+    <button onClick={colm2}> </button>
+    
+    <button onClick={colm3}> </button>
+
+    </div>
+
+    )
+};
+export default Row;
